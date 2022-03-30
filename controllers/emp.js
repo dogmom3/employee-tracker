5d94a4f2262987081587b73a527e0221d28a3b8b
dogmom3 <blair.seivers@yahoo.com>
2022-03-30 09:36:14 -0400

    quick update

--- controllers/emp.js
+++ controllers/emp.js
@@ -9,98 +9,105 @@ const viewEmployees = () => {
 
 //inserting answers into the database
 const addEmployee = async () => {
-   const answer = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'empFirstName',
-            message: 'What is the first name of the employee?',
-            validate: userInput => {
-                if (userInput) {
-                    return true;
-                } else {
-                    console.log('You must enter a first name for this employee.');
-                    return false;
+    try {
+        const answer = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'empFirstName',
+                message: 'What is the first name of the employee?',
+                validate: userInput => {
+                    if (userInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter a first name for this employee.');
+                        return false;
+                    }
                 }
-            }
-        },
+            },
 
-        {
-            type: 'input',
-            name: 'empLastName',
-            message: 'What is the last name of the employee?',
-            validate: userInput => {
-                if (userInput) {
-                    return true;
-                } else {
-                    console.log('You must enter a last name for this employee.');
-                    return false;
+            {
+                type: 'input',
+                name: 'empLastName',
+                message: 'What is the last name of the employee?',
+                validate: userInput => {
+                    if (userInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter a last name for this employee.');
+                        return false;
+                    }
                 }
-            }
-        },
+            },
 
-        {
-            type: 'input',
-            name: 'empRole',
-            message: 'What is the role of the employee?',
-            validate: userInput => {
-                if (userInput) {
-                    return true;
-                } else {
-                    console.log('You must enter a role for this employee.');
-                    return false;
+            {
+                type: 'input',
+                name: 'empRole',
+                message: 'What is the role of the employee?',
+                validate: userInput => {
+                    if (userInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter a role for this employee.');
+                        return false;
+                    }
                 }
-            }
-        },
+            },
 
-        {
-            type: 'input',
-            name: 'empManager',
-            message: 'Who is the manager for the employee?',
-            validate: userInput => {
-                if (userInput) {
-                    return true;
-                } else {
-                    console.log('You must enter a manager for this employee.');
-                    return false;
+            {
+                type: 'input',
+                name: 'empManager',
+                message: 'Who is the manager for the employee?',
+                validate: userInput => {
+                    if (userInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter a manager for this employee.');
+                        return false;
+                    }
                 }
-            }
-        },
-    ])
-        // .then((answer) => {
-            firstName = answer.empFirstName
-            lastName = answer.empLastName
-            role = answer.empRole
-            manager = answer.empManager
-            const sqlQuery = `INSERT INTO employees {first_name, last_name, role_id, manager_id} VALUES ("${firstName}","${lastName}","${role}","${manager}")`;
-            return db.query(sqlQuery, answer.empFirstName, answer.empLastName, answer.empRole, answer.empManager)
-        // })
+            },
+
+        ])
+        firstName = answer.empFirstName
+        lastName = answer.empLastName
+        role = answer.empRole
+        manager = answer.empManager
+        const sqlQuery = `INSERT INTO employees {first_name, last_name, role_id, manager_id} VALUES ("${firstName}","${lastName}","${role}","${manager}")`;
+        return db.query(sqlQuery, answer.empFirstName, answer.empLastName, answer.empRole, answer.empManager)
+
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 //updating employee role
 const updateEmployeeRole = async () => {
-    await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'employee_id',
-            message: 'Provide employee id to update.'
-        },
-        {
-            type: 'input',
-            name: 'updateRoleId',
-            message: 'Provide a new role id for the employee.',
-        }
-    ])
-    .then((answer) => {
+    // try {
+        const answer = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'employee_id',
+                message: 'Provide employee id to update.'
+            },
+            {
+                type: 'input',
+                name: 'updateRoleId',
+                message: 'Provide a new role id for the employee.',
+            }
+        ])
         let employee_id = answer.employee_id
         let updateRoleId = answer.updateRoleId
         const sqlQuery = `UPDATE employee SET role_id = ${updateRoleId} WHERE id = ${employee_id}`
-        db.query(sqlQuery)
-    })
-}
+        return db.query(sqlQuery)
+    }
+    // } catch (err) {
+    //     console.log(err);
+    // }
 
-//exporting the employee data
-module.exports = {
-    viewEmployees: viewEmployees,
-    addEmployee: addEmployee,
-    updateEmployeeRole: updateEmployeeRole
-}
+
+    //exporting the employee data
+    module.exports = {
+        viewEmployees: viewEmployees,
+        addEmployee: addEmployee,
+        updateEmployeeRole: updateEmployeeRole
+    }
